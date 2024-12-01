d41f5eb528f07da2093f46191db79c63adb47555
ArwaaLabs <business@arwaalab.com>
2024-12-01 10:26:56 +0530

    Fixed Eslint error

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      requireConfigFile: false,
+    },
+    extends: ['next/core-web-vitals'],
+  };
+  
